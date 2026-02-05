e3552f5884252728dd1ce20d7e958f6681f0773a
Chaimcv <chaithanyamcv@gmail.com>
2026-02-05 14:31:10 +0530

    updates

--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -31,9 +31,25 @@ const CommentsContainer = () => {
 }
   ];
 
+  const Comment=({data}) =>{
+    const { name, text, replies }= data;    //extracting(destructuring) name,text and replies 
+    return <div className="flex bg-gray-100">
+         <img
+          className="h-5"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="user-icon"
+        ></img>
+        <div className="px-3">
+            <p className="font-semibold">{name}</p>
+             <p>{text}</p>
+        </div>
+        </div>
+  }
+
   return (
     <div className="m-2 p-2">
       <h1 className="font-bold">Comments</h1>
+      <Comment data={commentsData}/>
     </div>
   );
 };
