33a8fcfed7115d6d9e4f807b4c7e820e8b279632
Chaimcv <chaithanyamcv@gmail.com>
2026-02-07 22:54:06 +0530

    live chat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import ChatMessages from './ChatMessages'
 import { useDispatch, useSelector } from 'react-redux'
 import { addMessage } from '../utils/chatSlice';
-import { randomMessages } from '../utils/helper';
+import { generateRandomName, makeRandomMessage } from '../utils/helper';
 
 const LiveChat = () => {
 const dispatch=useDispatch();
@@ -15,14 +15,14 @@ const chatMessages =useSelector((store)=> store.chat.messages);
          console.log("apipolling");
          console.log(chatMessages);
          dispatch(addMessage({
-            name:chatMessages,
-            message:"ss",
+            name:generateRandomName(),
+            message:makeRandomMessage(10),
          }))
         }, 2000);
         return ()=> clearInterval(i);
     })
   return (
-    <div className='rounded-lg pl-3 overflow-y-scroll'>
+    <div className='rounded-lg  h-[500] pl-3 overflow-y-scroll'>
         {chatMessages.map((chatmsg,index)=>(
  <ChatMessages key={index} name={chatmsg.name} messsage={chatmsg.message} />
         ))}
